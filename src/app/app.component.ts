50961f13d084dd7e66ecd254c7f938fdf6d9dcfc
Biki101 <thakurbiki52@gmail.com>
2025-09-13 15:04:59 +0545

    added table loading user search

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { UserService } from './services/user.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -10,32 +11,65 @@ export class AppComponent {
   isLoading = false;
   userList: any[] = [];
   pagedList: any[] = [];
+  filteredUserList: any[] = [];
   errorMessage: string = '';
   page = 1;
   pageSize = 5;
+  searchQuery = '';
 
-  constructor(private apiService: UserService) {}
+  constructor(
+    private apiService: UserService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) {}
 
   ngOnInit() {
     this.getUserList();
   }
 
   getUserList() {
+    this.isLoading = true;
     this.apiService.getUserList().subscribe({
       next: (res) => {
         this.userList = res;
+        this.filteredUserList = this.userList;
         this.pagedItems();
+        this.isLoading = false;
       },
       error: (err) => {
         this.errorMessage = 'Failed to load user list. Please try again later.';
+        this.isLoading = false;
+      },
+      complete: () => {
+        this.isLoading = false;
       },
     });
+  }
+
+  filterUserList() {
+    if (this.searchQuery?.trim() === '') {
+      this.filteredUserList = this.userList;
+    } else {
+      this.filteredUserList = this.userList.filter(
+        (user) =>
+          user.name.toLowerCase().includes(this.searchQuery.toLowerCase()) ||
+          user.email.toLowerCase().includes(this.searchQuery.toLowerCase())
+      );
+    }
+
+    this.router.navigate([], {
+      relativeTo: this.activatedRoute,
+      queryParams: { search: this.searchQuery },
+      queryParamsHandling: 'merge',
+    });
+
+    this.pagedItems();
   }
 
   pagedItems() {
     const start = (this.page - 1) * this.pageSize;
     const end = this.page * this.pageSize;
-    this.pagedList = this.userList.slice(start, end);
+    this.pagedList = this.filteredUserList.slice(start, end);
   }
 
   onPageChange(event: number) {
