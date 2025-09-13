3356a1cc294ee5331f639fb860490ff96bdae97f
Biki101 <thakurbiki52@gmail.com>
2025-09-13 14:31:25 +0545

    added pagination

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,10 @@ import { UserService } from './services/user.service';
 export class AppComponent {
   isLoading = false;
   userList: any[] = [];
+  pagedList: any[] = [];
   errorMessage: string = '';
+  page = 1;
+  pageSize = 5;
 
   constructor(private apiService: UserService) {}
 
@@ -21,10 +24,22 @@ export class AppComponent {
     this.apiService.getUserList().subscribe({
       next: (res) => {
         this.userList = res;
+        this.pagedItems();
       },
       error: (err) => {
         this.errorMessage = 'Failed to load user list. Please try again later.';
       },
     });
+  }
+
+  pagedItems() {
+    const start = (this.page - 1) * this.pageSize;
+    const end = this.page * this.pageSize;
+    this.pagedList = this.userList.slice(start, end);
+  }
+
+  onPageChange(event: number) {
+    this.page = event;
+    this.pagedItems();
   }
 }
