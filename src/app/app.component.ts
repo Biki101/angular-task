29d214a37622c419d3da658eddf04ffbbcfb2369
Biki101 <thakurbiki52@gmail.com>
2025-09-13 14:10:25 +0545

    added userlist api and error message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { UserService } from './services/user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-task';
+  isLoading = false;
+  userList: any[] = [];
+  errorMessage: string = '';
+
+  constructor(private apiService: UserService) {}
+
+  ngOnInit() {
+    this.getUserList();
+  }
+
+  getUserList() {
+    this.apiService.getUserList().subscribe({
+      next: (res) => {
+        this.userList = res;
+      },
+      error: (err) => {
+        this.errorMessage = 'Failed to load user list. Please try again later.';
+      },
+    });
+  }
 }
